a6bce6a10bfabfd7d90a8e373d9640e78faa36cb
hugo-sants <hugosantos30@gmail.com>
2026-03-23 16:10:27 -0300

    feat: initial API structure

---
+++ nexus-stock-api/src/main/java/com/sants/nexus_stock_api/NexusStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.sants.nexus_stock_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusStockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusStockApiApplication.class, args);
+	}
+
+}
---
+++ nexus-stock-api/src/test/java/com/sants/nexus_stock_api/NexusStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sants.nexus_stock_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
